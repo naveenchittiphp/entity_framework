7408cd14c28b320125cdec0404ed696a5642ba0a
naveenchittiphp <74538838+naveenchittiphp@users.noreply.github.com>
2021-12-29 10:11:59 +0530

    Create 1Operations_in_entity.php

---
+++ 1Operations_in_entity.php
@@ -0,0 +1,39 @@
+Select :  
+First define the database connection: 
+
+ projectDatabaseContext db = new projectDatabaseContext(); 
+
+Do operations : 
+
+var userAndRole = db.AspNetUserRoles.Where(x => (x.UserId == UserId)).First();
+
+Add : 
+
+  Audit auditLogs = new Audit();
+                                auditLogs.Username = username;
+                                auditLogs.AuditDateTime = DateTime.UtcNow;
+                                auditLogs.AuditEventType = "Coding";
+                                auditLogs.AuditEventDescription = "Reviewed";
+                                auditLogs.Bdosid = model.AssignCodingInquiryCases[a].BDOSID;
+                                auditLogs.PatientId = int.Parse(dtPatient.Rows[0]["PatientID"].ToString());
+                                auditLogs.NoteText = model.AssignCodingInquiryCases[a].ReviewedComments;
+                                auditLogs.UpdateFrom = userRoleDetais.Name;
+                                db.Audit.Add(auditLogs);
+
+-----------------
+Update : 
+---------------
+
+  var codingInquiry = db.CodingInquiry.Where(x => (x.Bdosid == model.AssignCodingInquiryCases[a].BDOSID)).First();
+                            codingInquiry.ReviewedComments = model.AssignCodingInquiryCases[a].ReviewedComments;
+                            codingInquiry.ReviewedStatus = "Reviewed";
+                            db.CodingInquiry.Update(codingInquiry);
+
+Delete : 
+----------
+
+ CodingInquiry rc = db.CodingInquiry.Where(x => x.Id == model.Id).SingleOrDefault();
+  db.CodingInquiry.Remove(rc);
+
+Save all changes to the database.
+ db.SaveChanges();
